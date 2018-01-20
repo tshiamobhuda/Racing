986eca25e31e7a971f3a762ad4cd087bc6f97c1a
Tshiamo Bhuda <tshiamobhuda@miway.co.za>
2018-01-20 20:39:40 +0200

    Added Enemy AI - Changed player movement to either be position 40|100 instead of using velocity to move player - set defualt enemy speed to 100 - Adding enemies into game every 2 seconds & killing all enemies once they pass world bounds - Randomly add an enemy at x either  40|100

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var mainState = {
     preload: function () {
         game.load.image('player', 'assets/player.png');
+        game.load.image('enemy', 'assets/enemy.png');
     },
 
     create: function () {
@@ -11,14 +12,32 @@ var mainState = {
         this.player = game.add.sprite(40, game.world.height - 80, 'player');
 
         this.cursor = game.input.keyboard.createCursorKeys();
+
+        this.speed = 100;
+
+        this.enemies = game.add.group();
+        this.timer = game.time.events.loop(2000, this.addEnemies, this);
     },
 
     update: function () {
         if (this.cursor.right.isDown)
-            this.player.body.velocity.x = 200;
+            this.player.body.x = 100;
         else if (this.cursor.left.isDown)
-            this.player.body.velocity.x = -200;
+            this.player.body.x = 40;
     },
+
+    addOneEmemy: function (x, y) {
+        var enemy = game.add.sprite(x, y, 'enemy');
+        this.enemies.add(enemy);
+
+        enemy.body.velocity.y = this.speed;
+        enemy.checkWorldBounds = true;
+        enemy.outOfBoundsKill = true;
+    },
+    
+    addEnemies: function () {
+        this.addOneEmemy(Phaser.Utils.randomChoice(40,100), 0);
+    }
 };
 
 var game = new Phaser.Game(200, 400, Phaser.CANVAS);
